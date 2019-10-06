94ffb0de3e93b9cce373207708ef0df5d34a542b
Sheeva <42568826+sheeva-r@users.noreply.github.com>
2019-10-07 05:12:07 +0900

    initial commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+function scrapeLodestone(){
+  var character = {
+    name: document.querySelector('.frame__chara__name').textContent,
+    realm: document.querySelector('.frame__chara__world').textContent.match(/^(\w*)\s+\((\w*)\)/),
+    server: "",
+    datacenter: "",
+    region: "",
+    fflogsid: "",
+  }
+  //console.log(character);
+  return character;
+};
+
+async function getFflogsId(character){
+    var regionMap = {
+      "Aether" : "NA",
+      "Crystal" : "NA",
+      "Primal" : "NA",
+      "Chaos" : "EU",
+      "Light" : "EU",
+      "Elemental" : "JP",
+      "Gaia" : "JP",
+      "Mana" : "JP",
+    };
+    character.server = character.realm[1];
+    character.datacenter = character.realm[2];
+    character.region = regionMap[character.datacenter];
+
+    var baseurl = 'https://www.fflogs.com:443/v1/rankings/character/';
+    var urlParam = encodeURI(character.name) + '/' + character.server + '/' + character.region + '?api_key=' + storedKey;
+    //console.log("API Request Parameter : " + urlParam);
+
+    await fetch(baseurl + urlParam)
+      .then(res => res.json())
+      .then(data => {
+          character.fflogsid = data[0].characterID;
+        })
+      .catch(function(error) {
+        console.log(error.message);
+        return -1;
+      });
+
+    return character.fflogsid;
+};
+
+async function addFflogsLinkElement(){
+  var url = "https://www.fflogs.com/character/id/";
+  var id = await getFflogsId(scrapeLodestone());
+  if(id < 1) return;
+  url += id;
+  //console.log("FFLogs Link URL : " + url);
+
+  var node = document.createElement("a");
+  node.setAttribute("class","badge badge-secondary ml-2 text-light");
+  node.setAttribute("href", url);
+  node.setAttribute("rel", "noopener");
+  node.innerHTML = "FFLogs"
+
+  document.getElementsByClassName('frame__chara__name')[0].appendChild(node);
+};
+
+var storedKey;
+chrome.storage.sync.get('fflogs_apikey', function(result){
+    storedKey = result.fflogs_apikey;
+    if (storedKey != "" ) addFflogsLinkElement();
+});
---
+++ js/popup.js
@@ -0,0 +1,28 @@
+var apikey;
+chrome.storage.sync.get('fflogs_apikey', function(result){
+      apikey = result.fflogs_apikey;
+      document.getElementById("fflogs-apikey-input").value = apikey;
+});
+
+const parent = document.getElementById("commentWrapper");
+const elem = document.createElement("p");
+const reference = document.getElementById("open-fflogs-link");
+
+window.addEventListener('load', function load(event) {
+  var button = document.getElementById('apply-button');
+  button.addEventListener('click', function() {
+    var input = document.getElementById('fflogs-apikey-input').value;
+    console.log(input);
+      if (input != "") {
+          chrome.storage.sync.set({'fflogs_apikey': input }, function() {
+              elem.textContent = "Saved your API Key.";
+              elem.setAttribute("class","alert alert-success");
+              parent.insertBefore(elem, reference);
+            });
+      } else {
+        elem.textContent = "field cannot be empty.";
+        elem.setAttribute("class","alert alert-warning");
+        parent.insertBefore(elem, reference);
+      };
+    }, false);
+});
